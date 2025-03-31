9f228323a38075ec3345188a01dd1fc17e5bdf0d
Dr31K0 <marshallgallagher037@gmail.com>
2025-03-31 13:56:16 +0200

    MeshStandard Delete

--- src/components/SuitcaseModel.tsx
+++ src/components/SuitcaseModel.tsx
@@ -74,7 +74,7 @@ const Model = () => {
                 mesh.material.needsUpdate = true;
                 console.log('Applied brightened color to:', mesh.name);
               } else {
-                console.log('Material is not MeshStandardMaterial or MeshPhysicalMaterial:', mesh.material);
+                console.log('Material is not MeshPhysicalMaterial:', mesh.material);
               }
             } else {
               console.log('Mesh has no material:', mesh.name);
